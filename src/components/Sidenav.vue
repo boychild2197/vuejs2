22a8d9a97006fdc0015edaf404693f2821558a3c
Boychild2197 <mshamugervas@gmail.com>
2022-01-26 14:28:28 +0300

    first commit

---
+++ src/components/Sidenav.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav>
+      <v-navigation-drawer v-model="sidenav"  color="primary" app :mini-variant.sync="mini" permanent>
+          <v-list dense>
+              <v-list-item>
+                  <v-list-item-avatar>
+                      <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+                  </v-list-item-avatar>
+                  <v-list-item-title>
+                      <span class="white--text display-0">John Doe</span>
+                  </v-list-item-title>
+              </v-list-item>
+              <v-divider></v-divider>
+              <v-list-item v-for="link in links" :key="link.text">
+                  <v-list-item-icon>
+                      <v-icon class="white--text">mdi-view-dashboard</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                      <v-list-item-title class="white--text">Dashboard</v-list-item-title>
+                  </v-list-item-content>
+              </v-list-item>
+          </v-list>
+      </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+        drawer:true,
+        mini:true,
+        
+        links:[
+            {name:'mdi-view-dashboard', text:'Dashboard'},
+            {name:'mdi-view-dashboard', text:'Dashboard'},
+            {name:'mdi-view-dashboard', text:'Dashboard'},
+            {name:'mdi-view-dashboard', text:'Dashboard'},
+        ]
+    }
+},
+props:['sidenav'],
+watch:{
+
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Topnav.vue
@@ -0,0 +1,42 @@
+<template>
+<v-container>
+    
+    <v-app-bar dense app>
+        <v-app-bar-nav-icon @click="changeSideNav" ></v-app-bar-nav-icon>
+        <v-app-bar-title class="success--text " >
+            <span>Boychild</span>
+        </v-app-bar-title>
+        <v-spacer></v-spacer>
+        <v-app-bar-title>
+            <v-btn text color="grey">
+                <span class="text-lowercase">profile</span>
+                <v-icon>mdi-account</v-icon>
+            </v-btn>
+        </v-app-bar-title>
+    </v-app-bar>
+    <sidenav :sidenav="Sidenav" />
+</v-container>
+</template>
+<script>
+import Sidenav from './Sidenav.vue';
+export default {
+    props:['v-model','sideNaV'],
+    components: { Sidenav },
+    
+    data(){
+        return{
+            Sidenav:true,
+        }
+    },methods:{
+        changeSideNav(){
+            this.Sidenav = !this.Sidenav
+            console.log(this.Sidenav);
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
